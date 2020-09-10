769c995627d71cadc454412a1a19601a7f4435fd
Feo-crypto <beerhunter922@gmail.com>
2020-09-10 10:25:59 +0300

    Human class work started

---
+++ src/Pract3Task2.java
@@ -0,0 +1,55 @@
+public class Pract3Task2 {
+
+    public class Human{
+        private class Head{
+            private int Health;
+            private boolean Crippled;
+        }
+        private class Limb{
+            private int Health;
+            private boolean Crippled;
+            public void UseDoctorsBag{
+                Health=100;
+                Crippled=false;
+            }
+        }
+        private class Torso{
+            private int Health;
+            private boolean Crippled;
+
+        }
+        private class RightArm{
+            private int Health;
+            private boolean Crippled;
+        }
+        private class LeftArm{
+            private int Health;
+            private boolean Crippled;
+        }
+        private class RightLeg{
+            private int Health;
+            private boolean Crippled;
+        }
+        private class LeftLeg{
+            private int Health;
+            private boolean Crippled;
+        }
+        private Head theHead=new Head();
+        private Torso theTorso=new Torso();
+        private RightArm theRightArm=new RightArm();
+        private LeftArm theLeftArm=new LeftArm();
+        private RightLeg theRightLeg=new RightLeg();
+        private LeftLeg theLeftLeg=new LeftLeg();
+
+
+        public void useDoctorsBag(){
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
