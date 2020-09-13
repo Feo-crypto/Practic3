0001ac833c1f0acb25506b830b1a6557a436d718
Feo-crypto <70689076+Feo-crypto@users.noreply.github.com>
2020-09-13 19:15:58 +0300

    Got "Human" Class working, Limb status has to be set with "SetLimbs" method, "useDoctorsBag" method restores health and condition of all limbs.

--- src/Pract3Task2.java
+++ src/Pract3Task2.java
@@ -1,55 +1,165 @@
 public class Pract3Task2 {
 
-    public class Human{
-        private class Head{
-            private int Health;
-            private boolean Crippled;
+    public static class Human{
+        protected int MaxHealth=100;
+        protected int Health;
+
+        public Human(){
+            setHealth(MaxHealth);
         }
-        private class Limb{
-            private int Health;
-            private boolean Crippled;
-            public void UseDoctorsBag{
-                Health=100;
+
+        protected class Limb{
+            protected int MaxHealth=100;
+            protected int Health=0;
+            protected boolean Crippled;
+            protected void Set(){
+                Health=MaxHealth;
+                Crippled=false;
+                CheckHealth();
+            }
+            protected void CheckHealth(){
+                if(Health<=0){
+                    Crippled=true;
+                }
+            }
+
+            public void UseDoctorsBag(){
+                Health=MaxHealth;
                 Crippled=false;
             }
-        }
-        private class Torso{
-            private int Health;
-            private boolean Crippled;
 
-        }
-        private class RightArm{
-            private int Health;
-            private boolean Crippled;
-        }
-        private class LeftArm{
-            private int Health;
-            private boolean Crippled;
-        }
-        private class RightLeg{
-            private int Health;
-            private boolean Crippled;
-        }
-        private class LeftLeg{
-            private int Health;
-            private boolean Crippled;
-        }
-        private Head theHead=new Head();
-        private Torso theTorso=new Torso();
-        private RightArm theRightArm=new RightArm();
-        private LeftArm theLeftArm=new LeftArm();
-        private RightLeg theRightLeg=new RightLeg();
-        private LeftLeg theLeftLeg=new LeftLeg();
+            public void setHealth(int health) {
+                Health = health;
+                CheckHealth();
+            }
 
+            public int getHealth() {
+                return Health;
+            }
+
+            public void setCrippled(boolean crippled) {
+                Crippled = crippled;
+            }
+
+            public void setMaxHealth(int maxHealth) {
+                MaxHealth = maxHealth;
+            }
+
+            public int getMaxHealth() {
+                return MaxHealth;
+            }
+            public String Status() {
+                return "Limb{" +
+                        "MaxHealth=" + MaxHealth +
+                        "/Health=" + Health +
+                        ", Crippled=" + Crippled +
+                        "}\n";
+            }
+        }
+
+        public void setMaxHealth(int maxHealth) {
+            MaxHealth = maxHealth;
+        }
+
+        public void setHealth(int health) {
+            Health = health;
+        }
+
+        class Head extends Limb{
+            public String Status() {
+            return "Head{" +
+                    "MaxHealth=" + MaxHealth +
+                    "/Health=" + Health +
+                    ", Crippled=" + Crippled +
+                    "}\n";
+        }
+        }
+        class Torso extends Limb{
+            public String Status() {
+            return "Torso{" +
+                    "MaxHealth=" + MaxHealth +
+                    "/Health=" + Health +
+                    ", Crippled=" + Crippled +
+                    "}\n";
+        }
+        }
+        class RightArm extends Limb{
+            public String Status() {
+            return "RightArm{" +
+                    "MaxHealth=" + MaxHealth +
+                    "/Health=" + Health +
+                    ", Crippled=" + Crippled +
+                    "}\n";
+        }
+        }
+        class LeftArm extends Limb{
+            public String Status() {
+            return "LeftArm{" +
+                    "MaxHealth=" + MaxHealth +
+                    "/Health=" + Health +
+                    ", Crippled=" + Crippled +
+                    "}\n";
+        }
+        }
+        class RightLeg extends Limb{
+            public String Status() {
+            return "RightLeg{" +
+                    "MaxHealth=" + MaxHealth +
+                    "/Health=" + Health +
+                    ", Crippled=" + Crippled +
+                    "}\n";
+        }
+        }
+        class LeftLeg extends Limb{
+            public String Status() {
+            return "LeftLeg{" +
+                    "MaxHealth=" + MaxHealth +
+                    "/Health=" + Health +
+                    ", Crippled=" + Crippled +
+                    "}\n";
+        }
+        }
+
+        public Head theHead=new Head();
+        public Torso theTorso=new Torso();
+        public RightArm theRightArm=new RightArm();
+        public LeftArm theLeftArm=new LeftArm();
+        public RightLeg theRightLeg=new RightLeg();
+        public LeftLeg theLeftLeg=new LeftLeg();
+
+        public void SetLimbs(){
+            theHead.Set();
+            theLeftArm.Set();
+            theRightArm.Set();
+            theLeftLeg.Set();
+            theRightLeg.Set();
+            theTorso.Set();
+        }
 
         public void useDoctorsBag(){
-
+            theHead.UseDoctorsBag();
+            theLeftArm.UseDoctorsBag();
+            theRightArm.UseDoctorsBag();
+            theLeftLeg.UseDoctorsBag();
+            theRightLeg.UseDoctorsBag();
+            theTorso.UseDoctorsBag();
+        }
+        public String FullStatus(){
+            return "{MaxHealth=" + MaxHealth +
+                    "/Health=" + Health+"}\n"+theHead.Status()+theTorso.Status()+theRightLeg.Status()+
+                    theLeftLeg.Status()+theRightArm.Status()+theLeftArm.Status();
         }
 
     }
 
 
     public static void main(String[] args) {
-
+        Human Giorno=new Human();
+        Giorno.SetLimbs();
+        System.out.println(Giorno.FullStatus());
+        Giorno.theHead.setHealth(0);
+        System.out.println(Giorno.FullStatus());
+        Giorno.useDoctorsBag();
+        System.out.println(Giorno.FullStatus());
     }
 }
